4e556fdea69931a0326d6bbf0574e6a847610b97
Paul Redmond <paulrredmond@gmail.com>
2014-09-06 12:29:18 -0700

    Initial calculate function with tests for basic operations.

--- calculator.js
+++ calculator.js
@@ -14,6 +14,68 @@
     }
 })('polishNotation', this, function () {
     var Calculator = function () {};
+
+    var Parser = {
+        parse: function (expression) {
+            var pieces = expression.split(/[\s\(\)]/),
+                start  = pieces.length - 1,
+                stack  = [];
+
+            for (var i = start, subject; i >= 0; i--) {
+                subject = pieces[i];
+                if (Parser.isAnOperand(subject) === true) {
+                    stack.unshift(parseFloat(subject));
+                } else if (Parser.isAnOperator(subject)) {
+                    stack.unshift(
+                        parseFloat(
+                            Parser._doOperation(subject, stack.splice(0,2))
+                        )
+                    );
+                }
+            }
+
+            return parseFloat(stack.shift());
+        },
+
+        _doOperation: function (operator, subjects) {
+            var x, y;
+            if (typeof subjects !== 'object' || subjects.length !== 2) {
+                throw new Error(
+                    'Operation requires two values. ' +
+                    'Had ' + subjects.length + 'values (' + subjects.join(' ') + ')'
+                );
+            }
+
+            x = parseInt(subjects.splice(0,1), 10);
+            y = parseInt(subjects.splice(0,1), 10);
+
+            return Parser[operator](x, y);
+        },
+
+        isAnOperand: function (subject) {
+            return (subject - parseFloat(subject) + 1) >= 0;
+        },
+
+        isAnOperator: function (subject) {
+            return /^[\+\*\/-]$/.test(subject);
+        },
+        "+": function (x, y) {
+            return x + y;
+        },
+        "-": function (x, y) {
+            return x - y;
+        },
+        "/": function (x, y) {
+            if (y === 0) {
+                throw new Error('Division by zero error.');
+            }
+
+            return x / y;
+        },
+        "*": function (x, y) {
+            return x * y;
+        }
+    };
     
     /**
      * Calculates a prefix notation string returning an integer value.
@@ -23,7 +85,7 @@
      * @return {number}            the calculated value.
      */ 
     Calculator.prototype.calculate = function (expression) {
-        return 3;
+        return Parser.parse(expression);
     };
 
     return Calculator;
--- tests/calculator.js
+++ tests/calculator.js
@@ -6,7 +6,27 @@ var chai = require('chai'),
 describe('Polish Notation Calculator', function () {
     describe ('calculate method', function () {
         it ('should add two numbers', function () {
-            expect(subject.calculate('(+ 1 2)'), '1 + 2').to.equal(3);
+            expect(subject.calculate('(+ 1 2)'), '1 + 2').to.eql(3);
+        });
+        
+        it ('should subtract two numbers', function () {
+            expect(subject.calculate('(- 10 6)')).to.eql(4);
+        });
+        
+        it ('should divide two numbers', function () {
+            expect(subject.calculate('(/ 10 2)')).to.eql(5);
+            expect(subject.calculate('(/ 1 5)')).to.eql(0.2);
+                    });
+
+        it ('should not allow division by zero', function () {
+            expect(function () {
+                subject.calculate('(/ 10 0)');
+            }).to.throw('Division by zero error.');
+        });
+        
+        it ('should multiply two numbers', function () {
+            expect(subject.calculate('(* 10 0)')).to.eql(0);
+            expect(subject.calculate('(* 10 5)')).to.eql(50);
         });
     });
 });
